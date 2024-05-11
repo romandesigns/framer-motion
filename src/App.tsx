7473faaa63abe3f50d33c0777876bacc4aeb5cd6
Roman F <wavystyledev@gmail.com>
2024-05-11 15:16:17 -0400

    animating grid and grid items

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { motion } from "framer-motion";
+
+const gridContainerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const gridItemVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <section className="flex flex-col gap-10 overflow-x-hidden w-full h-full">
+      <motion.section
+        variants={gridContainerVariants}
+        initial="hidden"
+        animate="show"
+        className="grid grid-cols-3 p-10 gap-10 border h-full"
+      >
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-sqare rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+      </motion.section>
+    </section>
   );
 }
 
